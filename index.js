1bb190b097fa985bb03745dd339db20dba562815
micolCODES <micolromano124@gmail.com>
2022-08-27 15:58:38 +0200

    - fix errors in package.json - change title - update movie database info

--- index.js
+++ index.js
@@ -15,120 +15,120 @@ let movies = [
     title: 'About Time',
     year: '2013',
     director:{
-      name: ,
-      birth: ,
-      death:
+      name: 'Richard Curtis',
+      birth: '1956',
+      death:''
     },
     actors: 'Rachel McAdams, Domnhall Gleeson, Bill Nighy',
-    genre: ,
+    genre: 'Romance',
     synopsis: ' The film is about a young man with the ability to time travel who tries to change his past in hopes of improving his future.'
   },
   {
     title: 'Grease',
     year: '1978',
     director:{
-      name: ,
-      birth: ,
-      death:
+      name: 'Randal Kleiser',
+      birth:'1946',
+      death:''
     },
     actors: 'Olivia Newton-John, John Travolta',
-    genre: ,
+    genre: 'Musical',
     synopsis: 'The musical depicts the lives of greaser Danny Zuko and Australian transfer student Sandy Olsson, who develop an attraction for each other during a summer romance.'
   },
   {
     title: 'Dirty Dancing',
     year: '1987',
     director:{
-      name: ,
-      birth: ,
-      death:
+      name: 'Emile Ardolino',
+      birth:'1943',
+      death:'1993'
     },
     actors: 'Jennifer Grey, Patrick Swayze',
-    genre: ,
+    genre: 'Romance',
     synopsis: 'The film tells the story of Frances "Baby" Houseman, a young woman who falls in love with dance instructor Johnny Castle at a vacation resort.'
   },
   {
     title: 'Coach Carter',
     year: '2005',
     director:{
-      name: ,
-      birth: ,
-      death:
+      name: 'Thomas Carter',
+      birth:'1953',
+      death:''
     },
     actors: 'Samuel L Jackson, Channing Tatum',
-    genre: ,
+    genre: 'Drama',
     synopsis: 'The film is based on the true story of Richmond High School basketball coach Ken Carter, who made headlines in 1999 for suspending his undefeated high school basketball team due to poor academic results.'
   },
   {
     title: 'The Blind Side',
     year: '2009',
     director:{
-      name: ,
-      birth: ,
-      death:
+      name: 'John Lee Hancock',
+      birth:'1956',
+      death:''
     },
     actors: 'Sandra Bullock, Tim McGraw',
-    genre: ,
+    genre: 'Drama',
     synopsis: 'The film tells the story of Michael Oher, an American football offensive lineman who overcame an impoverished upbringing to play in the National Football League (NFL) with the help of his adoptive parents Sean and Leigh Anne Tuohy'
   },
   {
     title: 'Avatar',
     year: '2009',
     director:{
-      name: ,
-      birth: ,
-      death:
+      name: 'James Cameron',
+      birth:'1954',
+      death:''
     },
     actors: 'Zoe Saldana, Sam Worthington, Sigourney Weaver',
-    genre: ,
+    genre: 'Fantasy',
     synopsis: 'The film is set in the mid-22nd century when humans are colonizing Pandora, a lush habitable moon of a gas giant in the Alpha Centauri star system, in order to mine the valuable mineral unobtanium. The expansion of the mining colony threatens the continued existence of a local tribe of Na\'vi – a humanoid species indigenous to Pandora.'
   },
   {
     title: 'The Lord of the Rings - Trilogy',
     year: '2001-2003',
     director:{
-      name: ,
-      birth: ,
-      death:
+      name: 'Peter Jackson',
+      birth:'1961',
+      death:''
     },
     actors: 'Elijah Wood, Ian McKellen, Liv Tyler, Viggo Mortensen, Sean Astin, Cate Blanchett, Christopher Lee, Orlando Bloom, Andy Serkis',
-    genre: ,
+    genre: 'Fantasy',
     synopsis: 'Set in the fictional world of Middle-earth, the films follow the hobbit Frodo Baggins as he and the Fellowship embark on a quest to destroy the One Ring, to ensure the destruction of its maker, the Dark Lord Sauron. The Fellowship eventually splits up and Frodo continues the quest with his loyal companion Sam and the treacherous Gollum. Meanwhile, Aragorn, heir in exile to the throne of Gondor, along with Legolas, Gimli, Boromir, Merry, Pippin and the wizard Gandalf, unite to save the Free Peoples of Middle-earth from the forces of Sauron and rally them in the War of the Ring to aid Frodo by distracting Sauron\'s attention.'
   },
   {
     title: 'Harry Potter - film series',
     year: '2001-2011',
     director:{
-      name: ,
-      birth: ,
-      death:
+      name: '[1-2] Chris Columbus, [3] Alfonso Cuarón, [4] Mike Newell, [5-8] David Yates',
+      birth:'1958, 1951, 1942, 1963',
+      death:''
     },
     actors: 'Emma Watson, Daniel Radcliffe, Rupert Grint',
-    genre: ,
+    genre: 'Fantasy',
     synopsis: 'The film\'s The main story arc concerns Harry, and young wizard, and his struggle against Lord Voldemort, a dark wizard who intends to become immortal, overthrow the wizard governing body known as the Ministry of Magic and subjugate all wizards and Muggles (non-magical people).'
   },
   {
     title: 'The Holiday',
     year: '2006',
     director:{
-      name: ,
-      birth: ,
-      death:
+      name: 'Nancy Meyers',
+      birth:'1949',
+      death:''
     },
     actors: 'Kate Winslet, Cameron Diaz, Jude Law',
-    genre: ,
+    genre: 'Romance',
     synopsis: 'Two women troubled with guy-problems swap homes in each other\'s countries, where they each meet a local guy and fall in love.'
   },
   {
     title: 'Pride and Prejudice',
     year: '2005',
     director:{
-      name: ,
-      birth: ,
-      death:
+      name: 'Joe Wright',
+      birth:'1971',
+      death:''
     },
     actors: 'Keira Knightly, Matthew Macfayden',
-    genre: ,
+    genre: 'Romance',
     synopsis: 'The film features five sisters from an English family of landed gentry as they deal with issues of marriage, morality and misconceptions.'
   }
 ];
@@ -165,7 +165,7 @@ app.delete('/movies/users/[username]', (req, res) => {
 
 // browser print something
 app.get('/', (req, res) => {
-  res.send('The first Rule of the Movie app is...');
+  res.send('Welcome to my Movie app');
 });
 
 app.get('/documentation', (req, res) => {                  
@@ -191,7 +191,6 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
 app.use(methodOverride());
-
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
