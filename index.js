f60bc52c333db9ac2b4207b1be8e85018bd1e4eb
micolCODES <micolromano124@gmail.com>
2022-09-07 13:11:19 +0200

    connect with Heroku and MongoDB Atlas

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/movieAppDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/movieAppDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://micolcodes:tVAFpod78zlU3XfM@micolsdb.p609irt.mongodb.net/movieAppDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
